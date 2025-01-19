c814a92a703e2532209b8f716e0aa50a53667caf
Dillan04 <dillanhammond7@gmail.com>
2025-01-19 21:23:43 +0100

    6th commit

--- index.js
+++ index.js
@@ -5,28 +5,42 @@ const print = console.log;
 const cors = require("cors");
 
 const shoppingRoutes = require("./api/shopping");
-const port = process.env.PORT || 8003
+const port = process.env.PORT || 8003;
 
 app.use(express.json());
-app.use(cors());
 app.use(express.static(__dirname + "/public"));
 
-const { CreateChannel} = require("./utils");
+// Define the allowed origins
+const allowedOrigins = ['https://multivendorapp-frontend.onrender.com', 'https://multivendorapp-user-microservice.onrender.com']; // Replace with your actual domains
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+  credentials: true, // Allow credentials (cookies, Authorization headers)
+};
+
+app.use(cors(corsOptions)); // Apply the CORS middleware with the custom options
+
+const { CreateChannel } = require("./utils");
 require("dotenv").config();
 app.use(express.urlencoded({ extended: true }));
 
 async function startApp() {
   try {
     await mongoose.connect(process.env.DB_URI);
-    print("Connection sauce");
+    print("Connection successful");
 
     const channel = await CreateChannel();
 
-    
-
     shoppingRoutes(app, channel);
     app.listen(port, () => {
-      console.log(`Order Service is Listening to Port ${port}`);
+      console.log(`Order Service is listening on Port ${port}`);
     });
   } catch (err) {
     console.log("Failed to start app:", err);
